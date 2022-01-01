d98ec7aac1eee10302ec682efcdaad67d76742b4
CouncilEstate <67240163+CouncilEstate@users.noreply.github.com>
2022-01-01 20:08:56 +0000

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,135 @@
+import 'dart:async';
+
+import 'package:agora_rtc_engine/rtc_engine.dart';
+import 'package:agora_rtc_engine/rtc_local_view.dart' as RtcLocalView;
+import 'package:agora_rtc_engine/rtc_remote_view.dart' as RtcRemoteView;
+import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+/// Define App ID and Token
+const APP_ID = "13576fbb30f44e0ea8b03f485ae6c500";
+const Token = "00613576fbb30f44e0ea8b03f485ae6c500IABrMV1c1xbfutMULBQrGqlYAEuh1f9oihFUQdWZgsKzQgx+f9gAAAAAEAA3+QZI54i8YQEAAQDkiLxh+f9gAAAAAEAAirequ46y7YQEAAQDjrLth";
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+// App state class
+class _MyAppState extends State<MyApp> {
+  bool _joined = false;
+  int _remoteUid = 0;
+  bool _switch = false;
+
+  @override
+  void initState() {
+    super.initState();
+    initPlatformState();
+  }
+
+  // Init the app
+  Future<void> initPlatformState() async {
+    await [Permission.camera, Permission.microphone].request();
+
+    // Create RTC client instance
+    RtcEngineContext context = RtcEngineContext(APP_ID);
+    var engine = await RtcEngine.createWithContext(context);
+    // Define event handling logic
+    engine.setEventHandler(RtcEngineEventHandler(
+        joinChannelSuccess: (String channel, int uid, int elapsed) {
+          print('joinChannelSuccess ${channel} ${uid}');
+          setState(() {
+            _joined = true;
+          });
+        }, userJoined: (int uid, int elapsed) {
+      print('userJoined ${uid}');
+      setState(() {
+        _remoteUid = uid;
+      });
+    }, userOffline: (int uid, UserOfflineReason reason) {
+      print('userOffline ${uid}');
+      setState(() {
+        _remoteUid = 0;
+      });
+    }));
+    // Enable video
+    await engine.enableVideo();
+    // Set channel profile as livestreaming
+    await engine.setChannelProfile(ChannelProfile.LiveBroadcasting);
+    // Set user role as broadcaster
+    await engine.setClientRole(ClientRole.Broadcaster);        
+    // Join channel with channel name as test
+    await engine.joinChannel(Token, 'test', null, 0);
+  }
+
+
+  // Build UI
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Watch Buddy'),
+        ),
+        body: Stack(
+          children: [
+            Center(
+              child: _switch ? _renderRemoteVideo() : _renderLocalPreview(),
+            ),
+            Align(
+              alignment: Alignment.topLeft,
+              child: Container(
+                width: 100,
+                height: 100,
+                color: Colors.blue,
+                child: GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      _switch = !_switch;
+                    });
+                  },
+                  child: Center(
+                    child:
+                    _switch ? _renderLocalPreview() : _renderRemoteVideo(),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  // Local preview
+  Widget _renderLocalPreview() {
+    if (_joined) {
+      return RtcLocalView.SurfaceView();
+    } else {
+      return Text(
+        'Please join channel first',
+        textAlign: TextAlign.center,
+      );
+    }
+  }
+
+  // Remote preview
+ Widget _renderRemoteVideo() {
+    if (_remoteUid != 0) {
+      return RtcRemoteView.SurfaceView(
+        uid: _remoteUid,
+        channelId: "test",
+      );
+    } else {
+      return Text(
+        'Please wait for remote user to join',
+        textAlign: TextAlign.center,
+      );
+    }
+  }
+
+}
